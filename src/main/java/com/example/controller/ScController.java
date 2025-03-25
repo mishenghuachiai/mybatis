fc4a5a1e75731e827724c54e18e2ff343212118f
MenBad <475333132@qq.com>
2025-03-25 22:47:55 +0800

    增加OpenFeign使用

--- src/main/java/com/example/controller/ScController.java
+++ src/main/java/com/example/controller/ScController.java
@@ -1,9 +1,12 @@
 package com.example.controller;
 
+import com.example.entity.Person;
 import com.example.service.ScService;
+import com.example.service.UserServiceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,18 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ScController {
 
-    @Value("${http_url}")
+    @Value("${http_url:192.168.99.100}")
     private String httpUrl;
     @Value("${name}")
     private String name;
     @Autowired
     ScService scService;
-
-    @RequestMapping("/home")
-    public String test() {
+    @Autowired
+    private UserServiceProvider userServiceProvider;
+    @RequestMapping("/home/{id}")
+    public Person test(@PathVariable int id) {
         System.out.println(httpUrl);
         System.out.println(name);
-//        scService.test();
-        return null;
+        /**
+         * 引入使用OpenFegin
+         */
+        userServiceProvider.getUser(id);
+        Person person = new Person();
+        person.setName(name);
+        return person;
     }
 }
